da17bd187b4abc0d0dac52ae2e36049986c6268b
David Marsland <david.marsland@gmail.com>
2017-12-20 13:26:41 -0800

    =added input to helloreactworld

--- helloreactworld/src/App.js
+++ helloreactworld/src/App.js
@@ -3,16 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { greeting: "from Mars!" };
+    this.onChange = this.onChange.bind(this);
+  };
+
+  onChange(event) {
+    this.setState({ greeting: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React {this.state.greeting}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input placeholder="Enter greeting" onChange={this.onChange} />
       </div>
     );
   }
